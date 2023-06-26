f7e6fe9258873375926a6f44dbe946a374b2f534
teammate <timvarsey@gmail.com>
2023-06-27 00:11:24 +0300

    complete 2-2

--- src/week2.ts
+++ src/week2.ts
@@ -44,16 +44,61 @@ function Purchase(){
 }
 
 // вывод в консоль
-Show()
-Add()
-console.log('Был добавлен товар')
-Show()
-Purchase()
-console.log('Товар был куплен')
-Show()
+// Show()
+// Add()
+// console.log('Был добавлен товар')
+// Show()
+// Purchase()
+// console.log('Товар был куплен')
+// Show()
 
 //! задание 2
+let check = [
+  {name: 'avocado', quantity: 2, price: 200},
+  {name: 'milk', quantity: 3, price: 80},
+  {name: 'chocolate', quantity: 1, price: 120},
+]
 
+function Print(){
+  for(let i of check){
+    console.log(i)
+  }
+}
+
+function Cost(){
+  let totalPrice = 0
+  for(let i of check){
+    totalPrice += i.price
+  }
+  console.log(`Стоимость покупки ${totalPrice} рублей`)
+}
+
+function maxPrice(){
+  let max = 0
+  let name = ''
+  for(let i of check){
+    if(i.price > max){
+      max = i.price
+      name = i.name
+    }
+  }
+  console.log(`Самый дорогой товар - ${name} (${max} рублей)`)
+}
+
+function averageCost(){
+  let sum = 0
+  for(let i of check){
+    sum += i.price
+  }
+  sum /= check.length
+  console.log(`Средняя стоимость одного товара ${Math.round(sum)} рубля`) 
+}
+
+// вывод в консоль
+Print()
+Cost()
+maxPrice()
+averageCost()
 
 //! задание 3
 
