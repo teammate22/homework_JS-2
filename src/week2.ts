c2426708ded48cf24397d06f3d3855a9790174f7
teammate22 <timvarsey@gmail.com>
2023-06-27 21:19:17 +0300

    complete 2-4 (not all)

--- src/week2.ts
+++ src/week2.ts
@@ -101,6 +101,89 @@ maxPrice()
 averageCost()
 
 //! задание 3
+let css = [
+  {name: 'font-size', value: '20px'},
+  {name: 'color', value: 'blue'},
+  {name: 'background-color', value: 'black'},
+  {name: 'text-decoration', value: 'underline'},
+  {name: 'width', value: '200px'},
+  {name: 'border-radius', value: '10px'},
+  {name: 'padding', value: '10px'},
+  {name: 'font-family', value: 'sans-serif'},
+]
+let p = document.getElementById('mainP')
+let style = document.getElementById('style')
 
+function useStyle(text: string){
+  p.innerHTML = `${text}`
+  style.innerHTML = `#mainP{
+    `
+  for(let i of css){
+    style.innerHTML += `${i.name}: ${i.value};
+    `
+  }
+  style.innerHTML += `}`
+}
+
+// вывод в консоль
+useStyle('I wrote styles for this element using type script')
 
 //! задание 4
+let classrooms = [
+  {name: 'yandex', seats: 15, faculty: 'web design'},
+  {name: 'apple', seats: 17, faculty: 'web development'},
+  {name: 'whatsapp', seats: 12, faculty: 'web design'},
+  {name: 'youtube', seats: 20, faculty: 'web development'},
+  {name: 'google', seats: 16, faculty: 'web development'},
+]
+let group = [
+  {name: 'design22', quantity: 15, faculty: 'web design'},
+  {name: 'web22-1', quantity: 17, faculty: 'web development'},
+  {name: 'web22-2', quantity: 14, faculty: 'web development'},
+]
+
+function ShowClassrooms(){
+  for(let i of classrooms){
+    console.log(i)
+  }
+}
+
+function ShowByFaculty(faculty: string){
+  for(let i of classrooms){
+    if(i.faculty == faculty){
+      console.log(i)
+    }
+  }
+}
+
+function FilterByGroup(groupName: string){
+  let groupQuantity = 0
+  let groupFaculty = ''
+  for(let i of group){
+    if(i.name == groupName){
+      groupQuantity = i.quantity
+      groupFaculty = i.faculty
+    }
+  }
+  for(let i of classrooms){
+    if(i.faculty == groupFaculty && i.seats >= groupQuantity){
+      console.log(i)
+    }
+  }
+}
+
+function SortBySeats(){
+  let a = classrooms.sort((a, b) => a.seats > b.seats ? 1 : -1)
+  console.log(a)
+}
+
+// вывод в консоль
+ShowClassrooms()
+console.log('Аудитория в которой преподают веб разработку')
+ShowByFaculty('web development')
+console.log('Какие аудитории подходят для группы web22-1')
+FilterByGroup('web22-1')
+console.log('Какие аудитории подходят для группы design22')
+FilterByGroup('design22')
+console.log('Сортировка аудиторий по количеству мест')
+SortBySeats()
